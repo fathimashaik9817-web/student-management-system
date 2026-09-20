c242641e94314b32f90b1d5e293d14f9109c7db9
fathima <fathimashaik9817-web@users.noreply.github.com>
2026-09-20 17:09:19 +0530

    Removed practice.py

--- practice.py
+++
@@ -1,9 +0,0 @@
-nums=(1,4,9,16,25,36,49,64,81,100)
-x=int(input("enter a number:"))
-i=0
-while i<=len(nums):
-    if(nums[i]==x):
-        print("found at idx",)
-    else:
-        print("founding..")
-    i+=1
